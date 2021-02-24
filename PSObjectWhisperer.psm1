9251e7a8f86d7b493fb57d81fa5fb6b6d01929f7
lucidhominid <lucidhominid@gmail.com>
2021-02-24 11:00:02 -0800

    Removed unnecessary lines from psm1 file.

--- PSObjectWhisperer.psm1
+++ PSObjectWhisperer.psm1
@@ -1,6 +1,4 @@
 $ModuleRootPath =  Split-Path -Path $MyInvocation.MyCommand.Path
-$ModuleName     = (Split-Path -Path $ModuleRootPath -Leaf) #-replace '\.ps1(?=$)'
-$Global:_PSAutoToolNamespace = Add-type -Path $ModuleRootPath\$ModuleName.cs -ReferencedAssemblies System.Drawing -PassThru
 
 Get-ChildItem $ModuleRootPath\Functions -filter *.ps1 | 
     ForEach-Object{
